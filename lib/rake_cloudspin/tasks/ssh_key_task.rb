b53bd9b439e8328f983c5c63a0db2565ed87c4ca
kief <kmorris@thoughtworks.com>
2018-06-19 16:39:13 +0100

    Fixed method name

--- lib/rake_cloudspin/tasks/ssh_key_task.rb
+++ lib/rake_cloudspin/tasks/ssh_key_task.rb
@@ -7,17 +7,17 @@ module RakeCloudspin
       def define
           desc "Ensure ssh keys for #{stack_name}"
           task :ssh_keys do
-            ssh_keys_config = stack_configuration.ssh_keys
+            ssh_keys_config = stack_config.ssh_keys
             ssh_keys_config.each { |ssh_key_name|
               key = AwsSshKey::Key.new(
-                key_path: "/#{stack_configuration.estate}/#{stack_configuration.component}/#{stack_name}/#{stack_configuration.deployment_identifier}/ssh_key",
+                key_path: "/#{stack_config.estate}/#{stack_config.component}/#{stack_name}/#{stack_config.deployment_identifier}/ssh_key",
                 key_name: ssh_key_name,
-                aws_region: stack_configuration.region,
+                aws_region: stack_config.region,
                 tags: {
-                  :Estate => stack_configuration.estate,
-                  :Component => stack_configuration.component,
+                  :Estate => stack_config.estate,
+                  :Component => stack_config.component,
                   :Service => stack_name,
-                  :DeploymentIdentifier => stack_configuration.deployment_identifier
+                  :DeploymentIdentifier => stack_config.deployment_identifier
                 }
               )
               key.load
