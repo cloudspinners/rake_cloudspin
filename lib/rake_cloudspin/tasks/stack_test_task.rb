76d8ffea7e48040e0b19a7675de721271fb2d2a7
kief <kmorris@thoughtworks.com>
2018-06-29 14:03:59 +0100

    Use the terraform variables as paramters for the inspec tests.

--- lib/rake_cloudspin/tasks/stack_test_task.rb
+++ lib/rake_cloudspin/tasks/stack_test_task.rb
@@ -3,16 +3,15 @@ module RakeCloudspin
   module Tasks
     class StackTestTask < BaseTask
 
-      parameter :stack_name, :required => true
-      parameter :stack_type, :required => true
+      parameter :stack_name
+      parameter :stack_type
 
       def define
         desc 'Run inspec tests'
-        task :test do
+        task :test do |t, args|
           if has_inspec_tests?
-            # TODO: Handle args that override configuration
-            create_inspec_attributes
-            run_inspec_profile
+            create_inspec_attributes.call(args)
+            run_inspec_profile.call(args)
           else
             puts "NO TESTS FOR STACK ('#{stack_name}')"
           end
@@ -24,27 +23,31 @@ module RakeCloudspin
       end
 
       def create_inspec_attributes
-        # TODO: Use args as overrides, so deployment_identifier will actually work
-        mkpath "work/tests/inspec"
-        File.open("work/tests/inspec/attributes-#{stack_type}-#{stack_name}.yml", 'w') {|f| 
-          f.write({
-            'deployment_identifier' => stack_config().deployment_identifier,
-            'component' => stack_config().component,
-            'service' => stack_name,
-            'stack_name' => stack_name
-          }.to_yaml)
-        }
+        lambda do |args|
+          mkpath "work/tests/inspec"
+          attributes_file_path = "work/tests/inspec/attributes-#{stack_type}-#{stack_name}.yml"
+          puts "INFO: Writing inspec attributes to file: #{attributes_file_path}"
+          File.open(attributes_file_path, 'w') {|f| 
+            f.write(test_attributes(args).to_yaml)
+          }
+        end
+      end
+
+      def test_attributes(args)
+        attributes = stack_config(args).vars
       end
 
       def run_inspec_profile
-        inspec_profiles = Dir.entries("#{stack_type}/#{stack_name}/tests/inspec").select { |profile|
-          profile != '..'
-          File.exists? ("#{stack_type}/#{stack_name}/tests/inspec/#{profile}/inspec.yml")
-        }.each { |profile|
-          profile_name = profile != '.' ? profile : '__root__'
-          puts "INSPEC (profile '#{profile_name}'): #{inspec_cmd(profile, profile_name)}"
-          system(inspec_cmd(profile, profile_name))
-        }
+        lambda do |args|
+          inspec_profiles = Dir.entries("#{stack_type}/#{stack_name}/tests/inspec").select { |profile|
+            profile != '..'
+            File.exists? ("#{stack_type}/#{stack_name}/tests/inspec/#{profile}/inspec.yml")
+          }.each { |profile|
+            profile_name = profile != '.' ? profile : '__root__'
+            puts "INSPEC (profile '#{profile_name}'): #{inspec_cmd(profile, profile_name)}"
+            system(inspec_cmd(profile, profile_name))
+          }
+        end
       end
 
       def inspec_cmd(profile, profile_name)
